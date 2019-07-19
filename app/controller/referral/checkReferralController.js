cd177956d00570e6054f9ba4ca1b2137bcaec466
Shatul Patil <coolshatul@gmail.com>
2019-07-19 17:57:57 +0530

    claimEvent

--- app/controller/referral/checkReferralController.js
+++ app/controller/referral/checkReferralController.js
@@ -56,39 +56,6 @@ module.exports = {
         console.log('========== Ref Done ==============');
     },
 
-    claimEvent: async function (req, res) {
-        let rules = {
-            "goal_code": 'required',
-        };
-        console.log(req.body);
-
-        let validation = new services.validator(req.body, rules);
-
-        if (validation.passes()) {
-
-            let _app_id = await services.commonServices.getAppId(req.headers["x-naz-app-key"]);
-            let _player_id = await services.commonServices.getPlayerIdByToken(req.headers["access-token"], _app_id);
-            let _goal_code = req.body.goal_code ? req.body.goal_code : null;
-
-            console.log('_app_id', _app_id);
-            console.log('_player_id', _player_id);
-            console.log('_goal_code', _goal_code);
-
-            if (_app_id && _player_id) {
-
-                services.sendResponse.sendWithCode(req, res, {}, customMsgType, "GET_SUCCESS");
-
-            } else {
-                services.sendResponse.sendWithCode(req, res, 'Invalid App Key or PlayerId', customMsgTypeCM, "VALIDATION_FAILED");
-
-            }
-
-        } else {
-            services.sendResponse.sendWithCode(req, res, validation.errors.errors, customMsgTypeCM, "VALIDATION_FAILED");
-
-        }
-    },
-
     getInviteCode: async function (req, res) {
 
         try {
@@ -190,4 +157,71 @@ module.exports = {
 
         }
     },
+
+    claimEvent: async function (req, res) {
+        let rules = {
+            "goal_code": 'required',
+        };
+        console.log(req.body);
+
+        let validation = new services.validator(req.body, rules);
+
+        if (validation.passes()) {
+
+            let _app_id = await services.commonServices.getAppId(req.headers["x-naz-app-key"]);
+            let _player_id = await services.commonServices.getPlayerIdByToken(req.headers["access-token"], _app_id);
+            let _goal_code = req.body.goal_code ? req.body.goal_code : null;
+
+            console.log('_app_id', _app_id);
+            console.log('_player_id', _player_id);
+            console.log('_goal_code', _goal_code);
+
+            if (_app_id && _player_id) {
+
+                let goal = await refModel.getGoals(_player_id, _app_id, _goal_code);
+
+                console.log(goal.length > 0, goal[0].goal_achieved);
+
+                if (goal && goal.length > 0 && goal[0].goal_achieved == 'false') {
+                    console.log('Goal need to achived');
+
+                    if (_goal_code == 'GAMEPLAY') {
+                        let x = checkGamePlay(goal[0])
+                    } else if (_goal_code == 'DEPOSIT') {
+
+                    } else {
+                        console.log('new goal code ', _goal_code);
+                    }
+
+                } else {
+                    console.log('no Goal to achived');
+                }
+
+                services.sendResponse.sendWithCode(req, res, goal, customMsgType, "GET_SUCCESS");
+
+            } else {
+                services.sendResponse.sendWithCode(req, res, 'Invalid App Key or PlayerId', customMsgTypeCM, "VALIDATION_FAILED");
+
+            }
+
+        } else {
+            services.sendResponse.sendWithCode(req, res, validation.errors.errors, customMsgTypeCM, "VALIDATION_FAILED");
+
+        }
+    },
+}
+
+
+function checkGamePlay(myGoal) {
+    let player_id = myGoal.player_id;
+    let app_id = myGoal.app_id;
+    let referred_by = myGoal.referred_by;
+    let goal_code = myGoal.goal_code;
+    let goal_achieved = myGoal.goal_achieved;
+    let reward_amount = myGoal.reward_amount;
+    let goal_achieved_from = myGoal.goal_achieved_from;
+    let goal_achieved_to = myGoal.goal_achieved_to;
+    let expiry_date = myGoal.expiry_date;
+
+    
 }
